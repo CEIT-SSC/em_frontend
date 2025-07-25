05b8feae57e8fb3ca1d7581a9e160ed5bca0da8a
amirhosseinaghighi <amirhossein.aghighi2@gmail.com>
2025-07-25 23:40:31 +0330

    Adds tailwind css

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,28 +1,37 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-
-import { join, dirname } from "path"
+import type { StorybookConfig } from "@storybook/react-vite";
+import { join, dirname } from "path";
 
 /**
-* This function is used to resolve the absolute path of a package.
-* It is needed in projects that use Yarn PnP or are set up within a monorepo.
-*/
-function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, 'package.json')))
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): string {
+  return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  "stories": [
+  stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
-  "addons": [
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/addon-docs'),
+  addons: [
+    getAbsolutePath("@chromatic-com/storybook"),
+    getAbsolutePath("@storybook/addon-docs"),
     getAbsolutePath("@storybook/addon-a11y"),
-    getAbsolutePath("@storybook/addon-vitest")
+    getAbsolutePath("@storybook/addon-vitest"),
   ],
-  "framework": {
-    "name": getAbsolutePath('@storybook/react-vite'),
-    "options": {}
-  }
+  framework: {
+    name: getAbsolutePath("@storybook/react-vite"),
+    options: {},
+  },
+  viteFinal: async (config) => {
+    return {
+      ...config,
+      css: {
+        postcss: {
+          plugins: [require("tailwindcss"), require("autoprefixer")],
+        },
+      },
+    };
+  },
 };
 export default config;
---
+++ packages/ui/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+import tailwindConfig from "@ssc/tailwind-config";
+
+export default {
+  ...tailwindConfig,
+  content: [
+    "./stories/**/*.{js,ts,jsx,tsx,mdx}",
+    "./.storybook/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    ...tailwindConfig.theme,
+  },
+  plugins: [],
+}
+
