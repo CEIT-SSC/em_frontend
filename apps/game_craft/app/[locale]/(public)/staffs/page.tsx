51103e64f4b8af94540094f1ea511a2cda10693e
Mahdi Haeri <mahdihaerim@gmail.com>
2025-09-02 01:34:53 +0330

    Fix bug

--- apps/game_craft/app/[locale]/(public)/staffs/page.tsx
+++ apps/game_craft/app/[locale]/(public)/staffs/page.tsx
@@ -1,15 +1,4 @@
 import { StaffView } from '@/components/shared/StaffView';
-import { getTranslations } from 'next-intl/server';
-
-export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
-  const { locale } = await params;
-  const t = await getTranslations({ locale, namespace: 'metadata' });
-
-  return {
-    title: t('staffs.title'),
-    description: t('staffs.description'),
-  };
-}
 
 export default function StaffsPage() {
   return <StaffView />;
