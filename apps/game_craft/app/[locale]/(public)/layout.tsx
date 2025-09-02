4e338d482342c28c77bc39ad2a0e97d50e6610fd
Mahdi Haeri <mahdihaerim@gmail.com>
2025-09-02 21:55:38 +0330

    CleanUp

--- apps/game_craft/app/[locale]/(public)/layout.tsx
+++ apps/game_craft/app/[locale]/(public)/layout.tsx
@@ -1,9 +1,43 @@
-import MainLayout from '@/components/layout/MainLayout'
+'use client'
 
-export default function PublicLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <MainLayout>{children}</MainLayout>
+import {Layout, theme} from 'antd'
+import {AppHeader} from "@/components/layout/AppHeader";
+import {AppFooter} from "@/components/layout/AppFooter";
+import Wave from "@/components/common/Wave";
+import {usePathname} from "next/navigation";
+
+interface MainLayoutProps {
+    children: React.ReactNode
+}
+
+const {useToken} = theme;
+
+export default function MainLayout({children}: MainLayoutProps) {
+    const {token} = useToken();
+    const pathname = usePathname();
+
+    // if pathname == /[locale]/ --> home page
+    const isHomePage = pathname === '/' || /^\/[a-zA-Z-]+\/?$/.test(pathname);
+
+    return (
+        <Layout style={{minHeight: '100vh', display: 'flex', flexDirection: 'column'}}>
+            <AppHeader/>
+            <Layout.Content style={{
+                flex: 1,
+                display: 'flex',
+                flexDirection: 'column',
+                backgroundColor: isHomePage ? token.colorBgBase : token.colorPrimary,
+                backgroundImage: isHomePage ? null : "url('/images/pattern.svg')",
+            }}>
+                {children}
+                <Wave
+                    width="100%"
+                    height="auto"
+                    fill={token.colorPrimary}
+                    style={{transform: "scaleY(-1) translateY(-2px)"}}
+                />
+            </Layout.Content>
+            <AppFooter/>
+        </Layout>
+    )
 }
