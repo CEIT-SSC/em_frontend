2644523c2b50c0a94829ffabe6240c3a1903939a
Pourya Fahimi <p.f.challenger369@gmail.com>
2025-09-11 14:09:12 +0330

    better responsive design

--- apps/ssc/app/(landings)/careers/components/JobList.tsx
+++ apps/ssc/app/(landings)/careers/components/JobList.tsx
@@ -5,9 +5,9 @@ const JobList = () => {
   return (
     <div className="flex flex-col gap-2.5 py-4 px-4 sm:px-12">
       <div className="flex flex-col gap-9 py-6 sm:px-8">
-        <JobOffer />
-        <JobOffer />
-        <JobOffer />
+        <JobOffer type="job" />
+        <JobOffer type="boot_camp" />
+        <JobOffer type="team" />
       </div>
     </div>
   );
