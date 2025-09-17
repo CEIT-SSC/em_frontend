7995dcd4011412754d0525087f84994c3b0c3281
Mahdi Haeri <mahdihaerim@gmail.com>
2025-09-17 04:42:20 +0330

    Fix bug

--- apps/game_craft/lib/api/index.ts
+++ apps/game_craft/lib/api/index.ts
@@ -10,7 +10,7 @@ declare module "axios" {
 
 // Create axios instance with base configuration
 const API = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "https://api.ceit-ssc.ir",
     headers: {
         "Content-Type": "application/json",
     },
