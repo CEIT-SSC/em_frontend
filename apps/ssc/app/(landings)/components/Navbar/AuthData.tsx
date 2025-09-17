58efc87c19bed83903114329f28633f302cebcb3
Pourya Fahimi <p.f.challenger369@gmail.com>
2025-09-18 00:29:32 +0330

    added logout button for ssc

--- apps/ssc/app/(landings)/components/Navbar/AuthData.tsx
+++ apps/ssc/app/(landings)/components/Navbar/AuthData.tsx
@@ -1,56 +1,67 @@
 "use client";
 
-import {Button, ButtonSize, ButtonVariant} from "@ssc/ui";
-import {useSession} from "next-auth/react";
+import { Button, ButtonSize, ButtonVariant } from "@ssc/ui";
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import {useEffect} from "react";
-import {userSelector} from "~/core/store/Auth/user.selector";
-import {fetchUserData} from "~/core/store/Auth/user.thunk";
-import {useAppDispatch, useAppSelector} from "~/core/store/store";
-import {AiOutlineLoading} from "react-icons/ai";
+import { useEffect } from "react";
+import { userSelector } from "~/core/store/Auth/user.selector";
+import { fetchUserData } from "~/core/store/Auth/user.thunk";
+import { useAppDispatch, useAppSelector } from "~/core/store/store";
+import { AiOutlineLoading } from "react-icons/ai";
 
 const AuthData = () => {
-    const dispatch = useAppDispatch();
-    const session = useSession();
-    const userData = useAppSelector(userSelector);
+  const dispatch = useAppDispatch();
+  const session = useSession();
+  const userData = useAppSelector(userSelector);
+  console.log(userData);
 
-    useEffect(() => {
-        if (session.status === "authenticated") {
-            dispatch(fetchUserData());
-        }
-    }, [dispatch, session]);
-
-    if (session.status === "unauthenticated") {
-        return (
-            <>
-                <Link href="/login">
-                    <Button
-                        className="min-h-9 min-w-16"
-                        variant={ButtonVariant.TEXT}
-                        size={ButtonSize.SMALL}
-                        label="ورود"
-                    />
-                </Link>
-                <Link href="/register">
-                    <Button
-                        className="min-h-9 min-w-16"
-                        variant={ButtonVariant.PRIMARY}
-                        size={ButtonSize.SMALL}
-                        label="ثبت نام"
-                    />
-                </Link>
-            </>
-        );
-    } else {
-        if (!userData.loggedIn || session.status === "loading") {
-            return <AiOutlineLoading className="animate-spin"/>;
-        }
-        return (
-            <>
-                <p>{userData.first_name + " " + userData.last_name}</p>
-            </>
-        );
+  useEffect(() => {
+    if (session.status === "authenticated") {
+      dispatch(fetchUserData());
     }
+  }, [dispatch, session]);
+
+  const handleLogout = () => {
+    signOut();
+  };
+
+  if (session.status === "unauthenticated") {
+    return (
+      <>
+        <Link href="/login">
+          <Button
+            className="min-h-9 min-w-16"
+            variant={ButtonVariant.TEXT}
+            size={ButtonSize.SMALL}
+            label="ورود"
+          />
+        </Link>
+        <Link href="/register">
+          <Button
+            className="min-h-9 min-w-16"
+            variant={ButtonVariant.PRIMARY}
+            size={ButtonSize.SMALL}
+            label="ثبت نام"
+          />
+        </Link>
+      </>
+    );
+  } else {
+    if (!userData.loggedIn || session.status === "loading") {
+      return <AiOutlineLoading className="animate-spin" />;
+    }
+    return (
+      <>
+        <p>{userData.first_name + " " + userData.last_name}</p>
+        <Button
+          className="min-h-9 min-w-16 bg-red-700"
+          size={ButtonSize.SMALL}
+          label="خروج از حساب"
+          onClick={handleLogout}
+        />
+      </>
+    );
+  }
 };
 
 export default AuthData;
--- apps/ssc/app/(landings)/components/Navbar/HamburgerMenu.tsx
+++ apps/ssc/app/(landings)/components/Navbar/HamburgerMenu.tsx
@@ -1,61 +1,63 @@
 "use client";
 
-import React, {useEffect, useState} from "react";
-import {HiMenu, HiX} from "react-icons/hi";
+import React, { useEffect, useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
 import clsx from "clsx";
 import AuthData from "./AuthData";
 import ActiveLink from "./ActiveLink";
-import {navbarItems} from "./Navbar";
-import {usePathname} from "next/navigation";
+import { navbarItems } from "./Navbar";
+import { usePathname } from "next/navigation";
 
 const HamburgerMenu = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const path = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const path = usePathname();
 
-    useEffect(() => {
-        setIsOpen(false);
-    }, [path]);
+  useEffect(() => {
+    setIsOpen(false);
+  }, [path]);
 
-    const menuLayout = clsx(
-        "fixed top-0 left-0 w-[100vw] h-[100vh] bg-(--MainGray)",
-        "z-99 flex flex-col *:h-full",
-        "transition-opacity !transition-500",
-        {
-            "opacity-100 pointer-events-auto": isOpen,
-            "opacity-0 pointer-events-none": !isOpen,
-        }
-    );
+  const menuLayout = clsx(
+    "fixed top-0 left-0 w-[100vw] h-[100vh] bg-(--MainGray)",
+    "z-99 flex flex-col *:h-full",
+    "transition-opacity !transition-500",
+    {
+      "opacity-100 pointer-events-auto": isOpen,
+      "opacity-0 pointer-events-none": !isOpen,
+    }
+  );
 
-    return (
-        <>
-            <button
-                className="cursor-pointer p-4 md:hidden"
-                onClick={() => setIsOpen(true)}
-            >
-                <HiMenu size={24}/>
-            </button>
-            <div className={menuLayout}>
-                <div className="p-6">
-                    <button
-                        className="cursor-pointer p-4"
-                        onClick={() => setIsOpen(false)}
-                    >
-                        <HiX size={24}/>
-                    </button>
-                </div>
-                <div className="flex flex-col gap-4 py-8 justify-center items-center text-2xl">
-                    {navbarItems.map((item) => (
-                        <ActiveLink href={item.href} key={item.label}>
-                            {item.label}
-                        </ActiveLink>
-                    ))}
-                </div>
-                <div className="flex justify-center items-center mb-8">
-                    <AuthData/>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <button
+        className="cursor-pointer p-4 md:hidden"
+        onClick={() => setIsOpen(true)}
+      >
+        <HiMenu size={24} />
+      </button>
+      <div className={menuLayout}>
+        <div className="p-6">
+          <button
+            className="cursor-pointer p-4"
+            onClick={() => setIsOpen(false)}
+          >
+            <HiX size={24} />
+          </button>
+        </div>
+        <div className="flex flex-col gap-4 py-8 justify-center items-center text-2xl">
+          {navbarItems.map((item) => (
+            <ActiveLink href={item.href} key={item.label}>
+              {item.label}
+            </ActiveLink>
+          ))}
+        </div>
+        <div className="flex justify-center items-center mb-8">
+          <div className="flex items-center gap-2">
+            <AuthData />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HamburgerMenu;
